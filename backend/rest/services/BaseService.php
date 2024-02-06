82cbf4d53f2745c4df0d6b1bd846e0f1bbe8ecc4
Ajla Korman <ajla.korman@stu.ibu.edu.ba>
2024-02-06 16:15:25 +0100

    Made additional changes

--- backend/rest/services/BaseService.php
+++ backend/rest/services/BaseService.php
@@ -8,6 +8,27 @@ class BaseService{
     
     }
 
+    public function get_all(){
+        return $this->dao->get_all();
+    }
+
+    public function get_by_id($id){
+        return $this->dao->get_by_id($id);
+    }
+
+    public function add($entity){
+        return $this->dao->add($entity); 
+        //umjesto da pisemo ovo u customerService, samo stavimo parent::add, i to se odnosi na ovu super klasu
+    }
+
+    public function update($entity, $id, $id_column = "id"){
+        return $this->dao->update($entity, $id, $id_column);
+    }
+    
+    public function delete($id){
+        return $this->dao->delete($id);
+    }
+
 }
 
 ?>
--- backend/rest/services/UserService.php
+++ backend/rest/services/UserService.php
@@ -20,21 +20,24 @@ class UserService extends BaseService
     }*/
 
 
-
     public function update($entity, $id, $id_column="id"){
         $entity['password'] = md5($entity['password']);
-        if(isset($id_column) && !is_null($id_column)){
-            return parent::update($entity, $id, $id_column);
+        if(isset($entity['id_column']) && !is_null($entity['id_column'])){
+            return parent::update($entity, $id, $entity['id_column']);
         }
         return parent::update($entity, $id);
     }
-    
-
 
     public function add($entity){
-        //unset($entity['phone']); ovo je da smo u form registration koloni imali i opciju da se unese phone, a nema ga u bazi
+       //unset($entity['phone']); //ovo je da smo u form registration koloni imali i opciju da se unese phone, a nema ga u bazi
        $entity['password'] = md5($entity['password']); //ovo je za hashing sifre
         return parent::add($entity);
+    }
+
+
+    function get_user_by_email($email)
+    {
+        return $this->dao->get_user_by_email($email);
     }
 
     //Ovo mi ne treba

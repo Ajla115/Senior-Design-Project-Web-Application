45b29d7730263d1f8b95155d4b00c8402b727d40
Ajla Korman <ajla.korman@stu.ibu.edu.ba>
2024-02-06 16:44:12 +0100

    Started with adding swagger documentation

---
+++ backend/rest/routes/ApiSpecs.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * @OA\Info(title="Senior Design Project", version="0.1", @OA\Contact(email="ajla.korman@stu.ibu.edu.ba", name="SDP Project"))
+ * @OA\OpenApi(
+ *    @OA\Server(url="http://127.0.0.1/Senior-Design-Project-Web-Application/backend/rest/", description="Development Environment" ),
+ *    @OA\Server(url="https://sea-lion-app-2bkxh.ondigitalocean.app/rest", description="Production Environment" )
+ * ),
+ * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
+ */
+
+
+?>

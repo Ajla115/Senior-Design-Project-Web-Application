6fef96b4e28fe6c5cb595d6a8c8b54ab88ece775
Ajla Korman <ajla.korman@stu.ibu.edu.ba>
2024-02-18 16:12:22 +0100

    Finished toggle password on register.js

--- frontend/src/pages/auth/register.js
+++ frontend/src/pages/auth/register.js
@@ -1,105 +1,96 @@
-import Head from 'next/head';
-import NextLink from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { Box, Button, Link, Stack, TextField, Typography } from '@mui/material';
-import { useAuth } from 'src/hooks/use-auth';
-import { Layout as AuthLayout } from 'src/layouts/auth/layout';
+import Head from "next/head";
+import NextLink from "next/link";
+import { useRouter } from "next/navigation";
+import { useFormik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+import {
+  Box,
+  Button,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
+import { useAuth } from "src/hooks/use-auth";
+import { Layout as AuthLayout } from "src/layouts/auth/layout";
+import { UserService } from "services";
+import { useQuery } from "@tanstack/react-query";
+
+import { Visibility, VisibilityOff } from "@mui/icons-material"; //needed for password toggling
 
 const Page = () => {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword); //setting the state of visibility
+  };
+
   const router = useRouter();
   const auth = useAuth();
   const formik = useFormik({
     initialValues: {
-      email: '',
-      first_name: '',
-      last_name: '',
-      password: '',
-      submit: null
+      email: "",
+      first_name: "",
+      last_name: "",
+      password: "",
+      submit: null,
     },
     validationSchema: Yup.object({
-      email: Yup
-        .string()
-        .email('Must be a valid email')
-        .max(255)
-        .required('Email is required'),
-      first_name: Yup
-        .string()
-        .max(255)
-        .required('First name is required'),
-        last_name: Yup
-        .string()
-        .max(255)
-        .required('Last name is required'),
-      password: Yup
-        .string()
-        .max(255)
-        .required('Password is required')
+      email: Yup.string().email("Must be a valid email").max(255).required("Email is required"),
+      first_name: Yup.string().max(255).required("First name is required"),
+      last_name: Yup.string().max(255).required("Last name is required"),
+      password: Yup.string().max(255).required("Password is required"),
     }),
     onSubmit: async (values, helpers) => {
       try {
-        await auth.signUp(values.email, values.first_name, values.last_name, values.password);
-        router.push('/');
+        console.log(values);
+        registerUser;
+        //await auth.signUp(values.email, values.first_name, values.last_name, values.password);
+        router.push("/");
       } catch (err) {
         helpers.setStatus({ success: false });
         helpers.setErrors({ submit: err.message });
         helpers.setSubmitting(false);
       }
-    }
+    },
   });
 
   return (
     <>
       <Head>
-        <title>
-          Register | InstaMetrics
-        </title>
+        <title>Register | InstaMetrics</title>
       </Head>
       <Box
         sx={{
-          flex: '1 1 auto',
-          alignItems: 'center',
-          display: 'flex',
-          justifyContent: 'center'
+          flex: "1 1 auto",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
         <Box
           sx={{
             maxWidth: 550,
             px: 3,
-            py: '100px',
-            width: '100%'
+            py: "100px",
+            width: "100%",
           }}
         >
           <div>
-            <Stack
-              spacing={1}
-              sx={{ mb: 3 }}
-            >
-              <Typography variant="h4">
-                Register
-              </Typography>
-              <Typography
-                color="text.secondary"
-                variant="body2"
-              >
-                Already have an account?
-                &nbsp;
-                <Link
-                  component={NextLink}
-                  href="/auth/login"
-                  underline="hover"
-                  variant="subtitle2"
-                >
+            <Stack spacing={1} sx={{ mb: 3 }}>
+              <Typography variant="h4">Register</Typography>
+              <Typography color="text.secondary" variant="body2">
+                Already have an account? &nbsp;
+                <Link component={NextLink} href="/auth/login" underline="hover" variant="subtitle2">
                   Log in
                 </Link>
               </Typography>
             </Stack>
-            <form
-              noValidate
-              onSubmit={formik.handleSubmit}
-            >
+            <form noValidate onSubmit={formik.handleSubmit}>
               <Stack spacing={3}>
                 <TextField
                   error={!!(formik.touched.first_name && formik.errors.first_name)}
@@ -139,17 +130,31 @@ const Page = () => {
                   label="Password"
                   name="password"
                   onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  type="password"
+                  //onChange={formik.handleChange}
+                  // type="password"
                   value={formik.values.password}
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => {
+                    formik.handleChange(e);
+                    // setPassword(e.target.value); --> this is no longer needed, because I included formik, and formik will take care of password field changes
+                  }}
+                  InputProps={{
+                    //inputProps is used to declare somthing that is additionaly used in the input field next to the data itself
+                    //endAdornment specifies the position of that icon in the field
+                    // edge="end" it is a good practice to have, because this ensures that no matter the number of line of code, the icon will always be located on the left
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleTogglePassword} edge="end">
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
                 />
               </Stack>
+
               {formik.errors.submit && (
-                <Typography
-                  color="error"
-                  sx={{ mt: 3 }}
-                  variant="body2"
-                >
+                <Typography color="error" sx={{ mt: 3 }} variant="body2">
                   {formik.errors.submit}
                 </Typography>
               )}
@@ -157,7 +162,7 @@ const Page = () => {
                 fullWidth
                 size="large"
                 sx={{ mt: 3 }}
-                type="submit"
+                // type="submit"
                 variant="contained"
               >
                 Register
@@ -170,10 +175,18 @@ const Page = () => {
   );
 };
 
-Page.getLayout = (page) => (
-  <AuthLayout>
-    {page}
-  </AuthLayout>
-);
+Page.getLayout = (page) => <AuthLayout>{page}</AuthLayout>;
 
 export default Page;
+
+function registerUser() {
+  console.log("test");
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ["users", "register"],
+    queryFn: UserService.register,
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+}
